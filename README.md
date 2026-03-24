71b822880d4b67b9ff80baae8bd25a377cda84c0
SuciuCalin <e.suciu.c@gmail.com>
2026-03-24 15:17:23 +0200

    added final

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Casa Inteligenta
-
-## Etc
---
+++ final/final.ino
@@ -0,0 +1,17 @@
+// Ce pini sunt folositi
+
+void setup() {
+  setup_usa_intrare();
+}
+
+void loop() {
+  loop_usa_intrare();
+}
+
+void setup_usa_intrare() {
+  // setarile initiale
+}
+
+void loop_usa_intrare() {
+  // toata logica din loop
+}
