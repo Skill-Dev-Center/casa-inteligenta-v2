a2de3a851f7ae2f62a0de0091327882a6579b3c5
flexiboard_64 <luisggpocas2013@gmail.com>
2026-03-25 18:56:50 +0200

    temperature_and_humidity
    temp and sensors 
    67

---
+++ temperatura_si_umiditate/temperatura_si_umiditate.ino
@@ -0,0 +1,15 @@
+void setup() {
+  setup_temp_sensors();
+}
+
+void loop() {
+  loop_temp_sensors();
+}
+
+void setup_temp_sensors() {
+  // setarile initiale
+}
+
+void loop_temp_sensors() {
+  // toata logica din loop
+}
