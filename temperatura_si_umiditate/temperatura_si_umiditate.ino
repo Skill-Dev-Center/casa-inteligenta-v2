33787bdbea25b832b2c709cc90a5295fe38b6684
flexiboard_64 <luisggpocas2013@gmail.com>
2026-04-01 18:43:21 +0300

    Add files via upload

--- temperatura_si_umiditate/temperatura_si_umiditate.ino
+++ temperatura_si_umiditate/temperatura_si_umiditate.ino
@@ -1,15 +1,37 @@
+#include <Wire.h>
+#include<DHT.h>
+#include<LiquidCrystal_I2C.h>
+int DHT_PIN = 11;
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+DHT dht(DHT_PIN, DHT11);
+
 void setup() {
-  setup_temp_sensors();
+setup_temp_sensors();
 }
 
 void loop() {
-  loop_temp_sensors();
+loop_temp_sensors();
 }
 
 void setup_temp_sensors() {
-  // setarile initiale
+delay(100);
+lcd.init();
+dht.begin();
+lcd.backlight();
 }
 
 void loop_temp_sensors() {
-  // toata logica din loop
+ 
+float temperatura =dht.readTemperature();
+float umiditate = dht.readHumidity();
+
+lcd.clear();
+lcd.print("Temp:");
+lcd.print(temperatura);
+lcd.setCursor(0,1);
+lcd.print("umid:");
+lcd.print(umiditate);
+delay(1000);
+
 }
