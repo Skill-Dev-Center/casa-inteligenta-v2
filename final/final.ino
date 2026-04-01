0db6460ddcedff662872df28acac4bc81aed834e
flexiboard_64 <luisggpocas2013@gmail.com>
2026-04-01 18:49:49 +0300

    Add files via upload

--- final/final.ino
+++ final/final.ino
@@ -1,22 +1,32 @@
 // Ce pini sunt folositi
 #include <Wire.h>
 #include <Servo.h>
+#include<DHT.h>
+#include<LiquidCrystal_I2C.h>
+
 
 Servo CADOURI_servomotor;
+
 
 const int CADOURI_SERVO_PIN = 4;
 const int CADOURI_BTN_PIN = 5;
 
 int CADOURI_unghi = 0;
+int DHT_PIN = 11;
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+DHT dht(DHT_PIN, DHT11);
 
 void setup() {
   setup_usa_intrare();
   cadouri_surpriza_setup();
+  setup_temp_sensors();
 }
 
 void loop() {
   loop_usa_intrare();
   cadouri_surpriza_loop();
+  loop_temp_sensors();
 }
 
 void setup_usa_intrare() {
@@ -53,3 +63,46 @@ void cadouri_surpriza_loop() {
   }
 
 }
+
+void setup_temp_sensors() {
+delay(100);
+lcd.init();
+dht.begin();
+lcd.backlight();
+}
+
+void loop_temp_sensors() {
+ 
+float temperatura =dht.readTemperature();
+float umiditate = dht.readHumidity();
+
+lcd.clear();
+lcd.print("Temp:");
+lcd.print(temperatura);
+lcd.setCursor(0,1);
+lcd.print("umid:");
+lcd.print(umiditate);
+delay(1000);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
