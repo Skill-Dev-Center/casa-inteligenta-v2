a7068e017cb122deab6f314fdf1c50620b397579
Suciu Eugen-Calin <e.suciu.c@gmail.com>
2026-04-28 18:11:59 +0300

    Add files via upload

--- final/final.ino
+++ final/final.ino
@@ -6,37 +6,47 @@
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 
+// PINS
 const int CADOURI_SERVO_PIN = 4;
 const int CADOURI_BTN_PIN = 5;
-const int DHT_PIN = 7;
+const int TEMP_SENSORS_DHT_PIN = 7;
+const int USA_INTRARE_BTN_PIN = 3;
+const int USA_INTRARE_SERVO_PIN = 8;
+const int USA_GARAJ_RFID_SS_PIN = 10;
+const int USA_GARAJ_RFID_RST_PIN = 9;
+const int USA_GARAJ_SERVO_PIN = 6;
 
-int CADOURI_unghi = 0;
-
+// COMPONENTS
 LiquidCrystal_I2C lcd(0x27,16,2);
-DHT dht(DHT_PIN, DHT11);
-
-#define RFID_SS_PIN 10
-#define RFID_RST_PIN 9
-#define SERVO_PIN 6
-
-MFRC522 rfid(RFID_SS_PIN, RFID_RST_PIN);
-Servo servoMotor;
+DHT dht(TEMP_SENSORS_DHT_PIN, DHT11);
+MFRC522 rfid(USA_GARAJ_RFID_SS_PIN, USA_GARAJ_RFID_RST_PIN);
+Servo USA_GARAJ_servoMotor;
 Servo CADOURI_servomotor;
+Servo USA_INTRARE_servomotor;
 
+// HELPERS
 String cardPermis[] = {"13", "93", "4B", "F5"};
+int usa_intrare_unghi = 0;
+int CADOURI_unghi = 0;
+bool USA_GARAJ_verify = false;
 
 void setup() {
+  // setup_usa_intrare();
+  cadouri_surpriza_setup();
   setup_temp_sensors();
   setup_usa_garaj();
-  cadouri_surpriza_setup();
 }
 
 void loop() {
+  // loop_usa_intrare();
   cadouri_surpriza_loop();
   loop_temp_sensors();
   loop_usa_garaj();
 }
 
+// =======================================
+// ============== USA GARAJ ==============
+// =======================================
 void setup_usa_garaj() {
   Serial.begin(9600);
 
@@ -45,104 +55,91 @@ void setup_usa_garaj() {
   rfid.PCD_Init();
 
   // Servo si LED-uri
-  servoMotor.attach(SERVO_PIN);
-  servoMotor.write(0);
+  USA_GARAJ_servoMotor.attach(USA_GARAJ_SERVO_PIN);
+  USA_GARAJ_servoMotor.write(0);
 
   Serial.println("Sistemul este pregatit! Apropie un card.");
 }
 
-void setup_usa_intrare() {
-  // setarile initiale
+void loop_usa_garaj() {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+    USA_GARAJ_verify = false;  
+  } 
+  else {
+    USA_GARAJ_verify = true;
+  }
+
+  if (USA_GARAJ_verify) {
+    Serial.print("Card scanat: ");
+    String cardID[4];
+
+    for (byte i = 0; i < 4; i++) {
+      cardID[i] = String(rfid.uid.uidByte[i], HEX);
+
+      if (cardID[i].length() == 1) {
+        cardID[i] = "0" + cardID[i];
+      }
+      cardID[i].toUpperCase();
+      Serial.print(cardID[i] + " ");
+    }
+
+    Serial.println();
+
+    // verificarea
+    bool cardCorect = true;
+    for (byte i = 0; i < 4; i++) {
+      if (cardID[i] != cardPermis[i]) {
+        cardCorect = false;
+        break;
+      }
+    }
+
+    if (cardCorect) {
+      Serial.println("Card corect!");
+      USA_GARAJ_servoMotor.write(90);
+      delay(3000);
+      USA_GARAJ_servoMotor.write(0);
+    }
+    else {
+      Serial.println("Card gresit!");
+    }
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+  }
 }
 
-void loop_usa_intrare() {
-  // toata logica din loop
-}
 
+// =======================================
+// ============= USA CADOURI =============
+// =======================================
 void cadouri_surpriza_setup() {
+  pinMode(CADOURI_BTN_PIN, INPUT_PULLUP);
   CADOURI_servomotor.attach(CADOURI_SERVO_PIN);
   CADOURI_servomotor.write(CADOURI_unghi);
-  delay(2000);
-  pinMode(CADOURI_BTN_PIN, INPUT_PULLUP);
-
-}
-
-void loop_usa_garaj() {
-  while (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
-
-  }
-  Serial.print("Card scanat: ");
-  String cardID[4];
-
-  for (byte i = 0; i < 4; i++) {
-    cardID[i] = String(rfid.uid.uidByte[i], HEX);
-
-    if (cardID[i].length() == 1) {
-      cardID[i] = "0" + cardID[i];
-    }
-    cardID[i].toUpperCase();
-    Serial.print(cardID[i] + " ");
-  }
-  Serial.println();
-
-  // verificarea
-  bool cardCorect = true;
-  for (byte i = 0; i < 4; i++) {
-    if (cardID[i] != cardPermis[i]) {
-      cardCorect = false;
-      break;
-    }
-  }
-
-  if (cardCorect) {
-    Serial.println("Card corect!");
-
-   
-    servoMotor.write(90);
-
-    delay(3000);
-
-    
-    servoMotor.write(0);
-  }
-  else {
-    Serial.println("Card gresit!");
-
-   
-}
-  rfid.PICC_HaltA();
-  rfid.PCD_StopCrypto1();
 }
 
 void cadouri_surpriza_loop() {
-   CADOURI_servomotor.write(CADOURI_unghi);
-
-   if (digitalRead(CADOURI_BTN_PIN) == LOW){
-
+  if (digitalRead(CADOURI_BTN_PIN) == LOW){
     CADOURI_unghi = 180;
-    
-
+    CADOURI_servomotor.write(CADOURI_unghi);
+    delay(3000);
   }
-
- 
-  
-  if (digitalRead(CADOURI_BTN_PIN) == HIGH){
-
+  if (digitalRead(CADOURI_BTN_PIN) == HIGH) {
     CADOURI_unghi = 0;
-
+    CADOURI_servomotor.write(CADOURI_unghi);
   }
-
 }
 
+// =======================================
+// ============= TENO & UMID =============
+// =======================================
 void setup_temp_sensors() {
-  delay(100);
   lcd.init();
   dht.begin();
   lcd.backlight();
 }
 
 void loop_temp_sensors() {
- 
   float temperatura =dht.readTemperature();
   float umiditate = dht.readHumidity();
 
@@ -153,5 +150,25 @@ void loop_temp_sensors() {
   lcd.print("umid:");
   lcd.print(umiditate);
   delay(1000);
+}
 
+// =======================================
+// ============= USA INTRARE =============
+// =======================================
+void setup_usa_intrare() {
+  pinMode(USA_INTRARE_BTN_PIN, INPUT_PULLUP);
+  USA_INTRARE_servomotor.attach(USA_INTRARE_SERVO_PIN);
+  USA_INTRARE_servomotor.write(usa_intrare_unghi);
+}
+
+void loop_usa_intrare() {
+  if (digitalRead(USA_INTRARE_BTN_PIN) == LOW) {
+    usa_intrare_unghi = 180;
+    delay(3000);
+    USA_INTRARE_servomotor.write(usa_intrare_unghi);
+  }
+  if (digitalRead(USA_INTRARE_BTN_PIN) == HIGH) {
+    usa_intrare_unghi = 0;
+    USA_INTRARE_servomotor.write(usa_intrare_unghi);
+  }
 }
